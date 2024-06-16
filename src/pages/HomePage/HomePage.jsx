5b9a5222a348416ffefbc1f0e6d0a65049a0b1e9
VladimirBerl <vova2002.13lol@gmail.com>
2024-06-16 11:43:17 +0300

    user name

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,33 @@
 import HomeTop from "../../components/HomeTop/HomeTop";
 import HomeIphone from "../../components/HomeIphone/HomeIphone";
 import styled from "./HomePage.module.scss";
-const tg = window.Telegram.WebApp
+import { useState, useEffect } from "react";
+const tg = window.Telegram.WebApp;
 export default function HomePage() {
-  function onClose() {
-    tg.close()
-  }
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    // Убедитесь, что Telegram WebApp API доступен
+    if (window.Telegram.WebApp) {
+      // Получение информации о пользователе
+      const tg = window.Telegram.WebApp;
+      const username = tg.initDataUnsafe.user?.username;
+      setUsername(username);
+    } else {
+      console.error("Telegram WebApp API недоступен");
+    }
+  }, []);
   return (
     <div>
       <HomeTop />
-      <HomeIphone/>
-      <button onClick={onClose}>dasd</button>
+      <HomeIphone />
+      <div>
+        {username ? (
+          <h1>Ваш ник: {username}</h1>
+        ) : (
+          <h1>Ник пользователя не найден</h1>
+        )}
+      </div>
     </div>
   );
 }
