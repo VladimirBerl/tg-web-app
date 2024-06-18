fec93aa015bf184eb1cbb5ce8b01711e79c385fa
VladimirBerl <vova2002.13lol@gmail.com>
2024-06-18 17:41:46 +0300

    upgreide counter

--- src/components/ui/Counter/Counter.jsx
+++ src/components/ui/Counter/Counter.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
-export default function Counter({ val, time = 1000 }) {
+export default function Counter({ val, time = 500}) {
   const [currVal, setCurrVal] = useState(0);
 
   useEffect(() => {
-    if(+currVal === +val) {
-      return 
-    } else {
-      setTimeout(setCurrVal(e => e + 1), time)
+    if (currVal !== val) {
+      const increment = Math.ceil((val - currVal) / (time / 50));
+      const timeout = setTimeout(() => setCurrVal(prev => Math.min(prev + increment, val)), 50);
+      return () => clearTimeout(timeout);
     }
-  }, [currVal]);
+  }, [currVal, val, time]);
 
   return <span>{currVal}</span>;
 }
