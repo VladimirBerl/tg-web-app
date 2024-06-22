4cc77c565123240d681952c82c73b6b490cc0e0f
VladimirBerl <vova2002.13lol@gmail.com>
2024-06-22 10:25:41 +0300

    add full page plan

--- src/components/PlanSwiper/PlanSwiper.jsx
+++ src/components/PlanSwiper/PlanSwiper.jsx
@@ -3,16 +3,164 @@ import { Carousel, ConfigProvider } from "antd";
 
 export default function PlanSwiper() {
   return (
-    <div className={styled.wrapper}>
-      <div className={styled.title}>
-        <p>Разработка</p>
-        <img src="public/ui/filter.svg" alt="" />
+    <div className={styled["wrapper-slider"]}>
+      {/* Разработка */}
+      <div className={styled.wrapper}>
+        <div className={styled.title}>
+          <p>Разработка</p>
+          <img src="public/ui/filter.svg" alt="" />
+        </div>
+        <Carousel infinite={false}>
+          <div className={styled["swaiper-items"]}>
+            <div className={styled["swaiper-item"]}>
+              <div>
+                <p>Сервис для покупателей /Продавцов маркетплейсов</p>
+                <img src="public/ui/check-round.svg" alt="check" />
+              </div>
+              <a href="https://t.me/Buyer_Marketplace">
+                BOT Buyer <img src="public/ui/bottom-right.svg" alt="right" />
+              </a>
+            </div>
+          </div>
+          <div className={styled["swaiper-items"]}>
+            <div className={styled["swaiper-item"]}>
+              <div>
+                <p>Группа Buyer Marketplace</p>
+                <img src="public/ui/check-round.svg" alt="check" />
+              </div>
+            </div>
+          </div>
+          <div className={styled["swaiper-items"]}>
+            <div className={styled["swaiper-item"]}>
+              <div>
+                <p>Бот TG Buyer - 5 модулей</p>
+                <img src="public/ui/check-round.svg" alt="check" />
+              </div>
+            </div>
+          </div>
+          <div className={styled["swaiper-items"]}>
+            <div className={styled["swaiper-item"]}>
+              <div>
+                <p>Чат для общения</p>
+                <img src="public/ui/check-round.svg" alt="check" />
+              </div>
+            </div>
+          </div>
+          <div className={styled["swaiper-items"]}>
+            <div className={styled["swaiper-item"]}>
+              <div>
+                <p>Чат для общения</p>
+                <img src="public/ui/check-round.svg" alt="check" />
+              </div>
+            </div>
+          </div>
+        </Carousel>
       </div>
-      <Carousel arrows infinite={false}>
-        <div className={styled["swaiper-item"]}>1</div>
-        <div className={styled["swaiper-item"]}>2</div>
-        <div className={styled["swaiper-item"]}>3</div>
-      </Carousel>
+      {/* Ликвидность */}
+      <div className={styled.wrapper}>
+        <div className={styled.title}>
+          <p>Ликвидность</p>
+          <img src="public/ui/shield.svg" alt="" />
+        </div>
+        <Carousel infinite={false}>
+          <div className={styled["swaiper-items"]}>
+            <div className={styled["swaiper-item"]}>
+              <div>
+                <p className={styled["title-yelow"]}>100 объявлений</p>
+              </div>
+              <p>Пул ликвидности в группе в день</p>
+            </div>
+          </div>
+          <div className={styled["swaiper-items"]}>
+            <div className={styled["swaiper-item"]}>
+              <div>
+                <p className={styled["title-green"]}>+130%</p>
+                <img src="public/ui/check-round.svg" alt="check" />
+              </div>
+              <p>230 объявлений в группе в день</p>
+            </div>
+          </div>
+        </Carousel>
+      </div>
+      {/* Маркетинг */}
+      <div className={styled.wrapper}>
+        <div className={styled.title}>
+          <p>Маркетинг</p>
+          <img src="public/ui/cart.svg" alt="" />
+        </div>
+        <Carousel infinite={false}>
+          <div className={styled["swaiper-items"]}>
+            <div className={styled["swaiper-item"]}>
+              <div>
+                <p className={styled["title-yelow"]}>50 объявлений</p>
+              </div>
+              <p>Бесплатные размещение в группе в месяц</p>
+            </div>
+          </div>
+          <div className={styled["swaiper-items"]}>
+            <div className={styled["swaiper-item"]}>
+              <div>
+                <p className={styled["title-red"]}>+26%</p>
+                <img src="public/ui/close-round.svg" alt="check" />
+              </div>
+              <p>13 объявлений в группе в месяц</p>
+            </div>
+          </div>
+        </Carousel>
+      </div>
+      {/* Реклама */}
+      <div className={styled.wrapper}>
+        <div className={styled.title}>
+          <p>Реклама</p>
+          <img src="public/ui/budget.svg" alt="" />
+        </div>
+        <Carousel infinite={false}>
+          <div className={styled["swaiper-items"]}>
+            <div className={styled["swaiper-item"]}>
+              <div>
+                <p className={styled["title-yelow"]}>50 объявлений</p>
+              </div>
+              <p>Бесплатные размещение в группе в месяц</p>
+            </div>
+          </div>
+          <div className={styled["swaiper-items"]}>
+            <div className={styled["swaiper-item"]}>
+              <div>
+                <p className={styled["title-red"]}>0%</p>
+                <img src="public/ui/close-round.svg" alt="check" />
+              </div>
+              <p>Объявлений в группе в месяц</p>
+            </div>
+          </div>
+        </Carousel>
+      </div>
+      {/* токен */}
+      <div className={styled.wrapper}>
+        <div className={styled.title}>
+          <p>Токен</p>
+          <img src="public/ui/token.svg" alt="" />
+        </div>
+        <Carousel infinite={false}>
+          <div className={styled["swaiper-items"]}>
+            <div className={styled["swaiper-item"]}>
+              <div>
+                <p className={styled["title-green"]}>Токена BMT</p>
+                <img src="public/ui/check-round.svg" alt="" />
+              </div>
+              <p>Использование в экосистеме Byer Marketplace</p>
+            </div>
+          </div>
+          <div className={styled["swaiper-items"]}>
+            <div className={styled["swaiper-item"]}>
+              <div>
+                <p className={styled["title-red"]}>0%</p>
+                <img src="public/ui/close-round.svg" alt="check" />
+              </div>
+              <p>Пока нет информации</p>
+            </div>
+          </div>
+        </Carousel>
+      </div>
     </div>
   );
 }
--- src/pages/PlanPage/PlanPage.jsx
+++ src/pages/PlanPage/PlanPage.jsx
@@ -1,10 +1,12 @@
-import PlanSwiper from "../../components/PlanSwiper/PlanSwiper"
-import styled from "./PlanPage.module.scss"
-export default function PlanPage () {
+import PlanSwiper from "../../components/PlanSwiper/PlanSwiper";
+import styled from "./PlanPage.module.scss";
+export default function PlanPage() {
   return (
-    <div className={styled.wrapper}>
-      <h3 className={styled.title}>Plan</h3>
-      <PlanSwiper/>
-    </div>
-  )
+    <>
+      <div className={styled.wrapper}>
+        <h3 className={styled.title}>Plan</h3>
+      </div>
+      <PlanSwiper />
+    </>
+  );
 }
