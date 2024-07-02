8bbdf701ead6f6ce4faa6fc2c7bb5e8b67956515
VladimirBerl <vova2002.13lol@gmail.com>
2024-07-02 22:32:10 +0300

    new friend page

--- src/components/FriendsListTask/FriendsListTask.jsx
+++ src/components/FriendsListTask/FriendsListTask.jsx
@@ -8,11 +8,31 @@ export default function FriendsListTask() {
         <div className={styled["swaiper-items"]}>
           <div className={styled["swaiper-item"]}>
             <div className={styled["task-item"]}>
-              <img src="/images/gift-box.png" alt="gift-box" />
+              <img src="/images/gift-box.svg" alt="gift-box" />
               <div>
                 <span className={styled["task-title"]}>Пригласить друга</span>
                 <div className={styled["task-text"]}>
-                  <img src="/images/coin.png" alt="coin" />
+                  <img src="/newUI/coin.svg" alt="coin" />
+                  <span>+5 000</span>
+                </div>
+                <div className={styled["task-wrapper"]}>
+                  <div className={styled["task-line"]}>
+                    <div></div>
+                  </div>
+                  <span>1/3</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styled["swaiper-items"]}>
+          <div className={styled["swaiper-item"]}>
+            <div className={styled["task-item"]}>
+              <img src="/images/gift-box.svg" alt="gift-box" />
+              <div>
+                <span className={styled["task-title"]}>Пригласить друга</span>
+                <div className={styled["task-text"]}>
+                  <img src="/newUI/coin.svg" alt="coin" />
                   <span>+5 000</span>
                 </div>
                 <div className={styled["task-wrapper"]}>
--- src/pages/FriendsPage/FriendsPage.jsx
+++ src/pages/FriendsPage/FriendsPage.jsx
@@ -3,46 +3,47 @@ import FriendsList from "../../components/FriendsList/FriendsList";
 import FriendsListTask from "../../components/FriendsListTask/FriendsListTask";
 export default function FriendsPage() {
   return (
-    <div className={styled.container}>
-      <div className={styled.wrapper}>
+    <>
+      <div className={styled["wrapper-top"]}>
         <h2 className={styled["wrapper-title"]}>Пригласите друзей!</h2>
         <span className={styled["wrapper-text"]}>
           Вы и ваш друг получит бонусы
         </span>
         <div className={styled.invite}>
           <div className={styled["invite-item"]}>
-            <img src="/images/gift-box.png" alt="gift-box" />
+            <img src="/images/gift-box.svg" alt="gift-box" />
             <div>
               <span className={styled["ivent-title"]}>Пригласить друга</span>
               <div className={styled["ivent-text"]}>
-                <img src="/images/coin.png" alt="coin" />
-                <span>+5 000</span>
-                для вас и вашего друга
+                <div>
+                  <img src="/newUI/coin.svg" alt="coin" />
+                  <span>+5 000</span>
+                </div>
+                <span>для вас и вашего друга</span>
               </div>
             </div>
           </div>
           <div className={styled["invite-item"]}>
-            <img src="/images/gift-box.png" alt="gift-box" />
+            <img src="/images/gift-box-two.svg" alt="gift-box" />
             <div>
               <span className={styled["ivent-title"]}>
                 Пригласить друга с Telegram Premium
               </span>
               <div className={styled["ivent-text"]}>
-                <img src="/images/coin.png" alt="coin" />
-                <span>+25 000</span>
-                для вас и вашего друга
+                <div>
+                  <img src="/newUI/coin.svg" alt="coin" />
+                  <span>+25 000</span>
+                </div>
+                <span>для вас и вашего друга</span>
               </div>
             </div>
           </div>
         </div>
       </div>
       <FriendsListTask />
-      <div className={styled.wrapper}>
+      <div className={styled["wrapper-bottom"]}>
         <FriendsList />
       </div>
-      <video autoPlay muted loop className={styled.video}>
-        <source src="/video/galactic-bg3.mp4" type="video/mp4" />
-      </video>
-    </div>
+    </>
   );
 }
