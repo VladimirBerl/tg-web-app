c8ef2a06d227b6af4d99049291624fc36f1010ca
VladimirBerl <vova2002.13lol@gmail.com>
2024-08-06 22:35:34 +0300

    update

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 import preloadResources from "@/shared/lib/preloadResources.js";
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     preloadResources(setIsLoading);
