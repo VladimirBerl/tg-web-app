8d782ad65640f29b09cd01ab69958bcc3d817c4e
VladimirBerl <vova2002.13lol@gmail.com>
2024-08-21 15:05:03 +0300

    updata

--- src/pages/HomePage/ui/HomeMain/ui/HomeCoin/ui/Coin/Coin.jsx
+++ src/pages/HomePage/ui/HomeMain/ui/HomeCoin/ui/Coin/Coin.jsx
@@ -1,5 +1,4 @@
 import styled from "./Coin.module.scss";
-<<<<<<< HEAD
 import { useState } from "react";
 import Info from "../Info/Info";
 const Coin = () => {
@@ -16,15 +15,6 @@ const Coin = () => {
       </div>
       {modal && <Info setShowModalBottom={setModal} />}
     </>
-=======
-
-const Coin = () => {
-  return (
-    <div className={styled.count}>
-      <img src="/icon/coin-min.png" alt="coin" />
-      <span>1</span>
-    </div>
->>>>>>> 34f800874e47c176a1c5fa5e63c364629c729cdb
   );
 };
 
