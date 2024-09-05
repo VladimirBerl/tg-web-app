83b67445cde9a82a4645a48f3b105b016dcaea85
VladimirBerl <vova2002.13lol@gmail.com>
2024-09-05 18:28:50 +0300

    updata

--- src/pages/ProfilPage/ui/ProfilUser/ui/UserName/UserName.jsx
+++ src/pages/ProfilPage/ui/ProfilUser/ui/UserName/UserName.jsx
@@ -1,12 +1,20 @@
+import { useEffect, useState } from "react";
 import styles from "./UserName.module.scss";
-window.Telegram.WebApp.ready();
-const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
-const userId = initDataUnsafe.user?.id;
+
 const UserName = ({ name }) => {
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    window.Telegram.WebApp.ready();
+    const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
+    const userIdFromTelegram = initDataUnsafe?.user?.id;
+    setUserId(userIdFromTelegram);
+  }, []);
+
   return (
     <div>
       <p className={styles.name}>{name}</p>
-      <p className={styles.name}>{userId}</p>
+      <p className={styles.name}>{userId ? userId : "Loading..."}</p>
     </div>
   );
 };
