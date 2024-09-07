63f0e51331081bab36b59ef5e5b3163341278c2b
VladimirBerl <vova2002.13lol@gmail.com>
2024-09-07 14:14:49 +0300

    updata

--- src/pages/EarnPage/ui/TaskItemList/ui/ButtonChek.jsx
+++ src/pages/EarnPage/ui/TaskItemList/ui/ButtonChek.jsx
@@ -37,7 +37,7 @@ const ButtonChek = ({ url, id }) => {
 
   useEffect(() => {
     setButtonText(localStorage?.taskButtonSate || "Перейти");
-    // chekSatusTask();
+    chekSatusTask();
   }, []);
 
   return (
