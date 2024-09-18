288203eb7bb37900b960cd9aa3c94cea423e1e5f
VladimirBerl <vova2002.13lol@gmail.com>
2024-09-18 09:45:26 +0300

    final updata link invate

--- src/pages/FriendsPage/ui/FriendsInvate/FriendsInvate.jsx
+++ src/pages/FriendsPage/ui/FriendsInvate/FriendsInvate.jsx
@@ -26,7 +26,7 @@ const FriendsInvate = () => {
 
   const sendInviteLink = () => {
     try {
-      const link = `https://t.me/share/url?url=%D0%9F%D1%80%D0%B8%D0%B3%D0%BB%D0%B0%D1%88%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%B2%20%D0%B4%D1%80%D1%83%D0%B7%D1%8C%D1%8F%20The%20Buyer:%20https%3A%2F%2Ft.me%2FTestapimybot%2F%3Fstart%3Dinvited_by_$%7Buser.id_telegram%7D`;
+      const link = `https://t.me/share/url?url=%D0%9F%D1%80%D0%B8%D0%B3%D0%BB%D0%B0%D1%88%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%B2%20%D0%B4%D1%80%D1%83%D0%B7%D1%8C%D1%8F%20The%20Buyer:%20https%3A%2F%2Ft.me%2FTestapimybot%2F%3Fstart%3Dinvited_by_${user.id_telegram}`;
       window.Telegram.WebApp.openTelegramLink(link);
     } catch (error) {
       console.log(error);
